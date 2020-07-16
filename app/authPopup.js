43a40eface815b181b189f1b7ab88301a8cc09df
Prithvi Kanherkar <prithvi.kanherkar@gmail.com>
2020-07-16 14:36:09 -0700

    update to beta.4, mostly working

--- app/authPopup.js
+++ app/authPopup.js
@@ -2,62 +2,77 @@
 // configuration parameters are located at authConfig.js
 const myMSALObj = new msal.PublicClientApplication(msalConfig);
 
-function signIn() {
-    myMSALObj.loginPopup(loginRequest)
-        .then(loginResponse => {
-            console.log('id_token acquired at: ' + new Date().toString());
-            console.log(loginResponse);
+let username = "";
 
-            if (myMSALObj.getAccount()) {
-                showWelcomeMessage(myMSALObj.getAccount());
-            }
-        }).catch(error => {
-            console.error(error);
-        });
+function handleResponse(resp) {
+    if (resp !== null) {
+        username = resp.account.username;
+        showWelcomeMessage(resp.account);
+    } else {
+        // need to call getAccount here?
+        const currentAccounts = myMSALObj.getAllAccounts();
+        if (currentAccounts === null) {
+            return;
+        } else if (currentAccounts.length > 1) {
+            // Add choose account code here
+            console.warn("Multiple accounts detected.");
+        } else if (currentAccounts.length === 1) {
+            username = currentAccounts[0].username;
+            showWelcomeMessage(currentAccounts[0]);
+        }
+    }
+}
+
+function signIn() {
+    myMSALObj.loginPopup(loginRequest).then(handleResponse).catch(error => {
+        console.error(error);
+    });
 }
 
 function signOut() {
-    myMSALObj.logout();
+    const logoutRequest = {
+        account: myMSALObj.getAccountByUsername(username)
+    };
+
+    myMSALObj.logout(logoutRequest);
 }
 
 function getTokenPopup(request) {
-    return myMSALObj.acquireTokenSilent(request)
-        .catch(error => {
-            console.warn(error);
-            console.warn("silent token acquisition fails. acquiring token using popup");
-
+    return myMSALObj.acquireTokenSilent(request).catch(error => {
+        console.warn("silent token acquisition fails. acquiring token using redirect");
+        if (error instanceof msal.InteractionRequiredAuthError) {
             // fallback to interaction when silent call fails
-            return myMSALObj.acquireTokenPopup(request)
-                .then(tokenResponse => {
-                    console.log(tokenResponse);
-                    
-                    return tokenResponse;
-                }).catch(error => {
-                    console.error(error);
-                });
-        });
+            return myMSALObj.acquireTokenPopup(request).then(tokenResponse => {
+                console.log(tokenResponse);
+                
+                return tokenResponse;
+            }).catch(error => {
+                console.error(error);
+            });
+        } else {
+            console.warn(error);   
+        }
+    });
 }
 
 function seeProfile() {
     if (myMSALObj.getAccount()) {
-        getTokenPopup(loginRequest)
-            .then(response => {
-                callMSGraph(graphConfig.graphMeEndpoint, response.accessToken, updateUI);
-                profileButton.classList.add('d-none');
-                mailButton.classList.remove('d-none');
-            }).catch(error => {
-                console.error(error);
-            });
+        getTokenPopup(loginRequest).then(response => {
+            callMSGraph(graphConfig.graphMeEndpoint, response.accessToken, updateUI);
+            profileButton.classList.add('d-none');
+            mailButton.classList.remove('d-none');
+        }).catch(error => {
+            console.error(error);
+        });
     }
 }
 
 function readMail() {
     if (myMSALObj.getAccount()) {
-        getTokenPopup(tokenRequest)
-            .then(response => {
-                callMSGraph(graphConfig.graphMailEndpoint, response.accessToken, updateUI);
-            }).catch(error => {
-                console.error(error);
-            });
+        getTokenPopup(tokenRequest).then(response => {
+            callMSGraph(graphConfig.graphMailEndpoint, response.accessToken, updateUI);
+        }).catch(error => {
+            console.error(error);
+        });
     }
 }
