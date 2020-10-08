06b777fc3be7959c900115112aee59ad8c5d1658
derisen <dogan.erisen@gmail.com>
2020-10-08 14:54:29 -0700

    auth config fix authority string slash character

--- app/authConfig.js
+++ app/authConfig.js
@@ -6,7 +6,7 @@
 const msalConfig = {
     auth: {
         clientId: "Enter_the_Application_Id_Here",
-        authority: "Enter_the_Cloud_Instance_Id_Here/Enter_the_Tenant_Info_Here",
+        authority: "Enter_the_Cloud_Instance_Id_HereEnter_the_Tenant_Info_Here",
         redirectUri: "Enter_the_Redirect_Uri_Here",
     },
     cache: {
