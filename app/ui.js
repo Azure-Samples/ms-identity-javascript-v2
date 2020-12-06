0f1376dbf9dc0e72cb75e0910d327e29791249cb
Doğan Erişen <dogan.erisen@gmail.com>
2020-12-05 17:16:32 -0800

    fix incorrect function argument

--- app/ui.js
+++ app/ui.js
@@ -6,10 +6,10 @@ const mailButton = document.getElementById("readMail");
 const profileButton = document.getElementById("seeProfile");
 const profileDiv = document.getElementById("profile-div");
 
-function showWelcomeMessage(account) {
+function showWelcomeMessage(username) {
     // Reconfiguring DOM elements
     cardDiv.style.display = 'initial';
-    welcomeDiv.innerHTML = `Welcome ${account.username}`;
+    welcomeDiv.innerHTML = `Welcome ${username}`;
     signInButton.setAttribute("onclick", "signOut();");
     signInButton.setAttribute('class', "btn btn-success")
     signInButton.innerHTML = "Sign Out";
