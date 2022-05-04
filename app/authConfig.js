112e66a0c3448ad35268532724bc7b4bc6f1cb27
Henry Mbugua <82150047+henrymbuguakiarie@users.noreply.github.com>
2022-05-04 21:12:27 +0300

    Update app/authConfig.js
    Co-authored-by: Doğan Erişen <dogan.erisen@gmail.com>

--- app/authConfig.js
+++ app/authConfig.js
@@ -8,7 +8,7 @@ const msalConfig = {
         // 'Application (client) ID' of app registration in Azure portal - this value is a GUID
         clientId: "Enter_the_Application_Id_Here",
         // Full directory URL, in the form of https://login.microsoftonline.com/<tenant>
-        authority: "Enter_the_Cloud_Instance_Id_Here/Enter_the_Tenant_Info_Here",
+        authority: "Enter_the_Cloud_Instance_Id_HereEnter_the_Tenant_Info_Here",
         // Full redirect URL, in form of http://localhost:3000
         redirectUri: "Enter_the_Redirect_Uri_Here",
     },
